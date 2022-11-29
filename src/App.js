5dbbc646c2c6b121507f37bd41cd087db13315c5
kabir <kabir.almuslim@gmail.com>
2022-11-29 19:28:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { Component } from "react";
+import "./App.css";
+import CardList from "./components/card-list/CardList";
+import SearchBox from "./components/search-box/SearchBox";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) =>
+        this.setState(() => {
+          return { monsters: user, searchField: "" };
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonster = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <h1 className="app-tittle">October Monsters</h1>
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="Search Monster"
+          className="searchBox"
+        />
+        <CardList monsters={filteredMonster} />
+      </div>
+    );
+  }
+}
+export default App;
